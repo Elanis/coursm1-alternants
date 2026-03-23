b67179e608022c8e88e658b976ccdac8e4a005f4
Elanis <elanis@hotmail.com>
2026-03-23 09:55:37 +0100

    useCallback example

--- src/context/CounterContext.jsx
+++ src/context/CounterContext.jsx
@@ -1,14 +1,19 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 
 export const CounterContext = createContext(null);
 
 export default function CounterContextProvider({ children }) {
 	const [counter, setCounter] = useState(0);
 
+	const increment = useCallback(() => {
+		setCounter((oldCounter) => oldCounter + 1);
+	}, [setCounter]);
+
 	return (
 		<CounterContext.Provider value={{
 			counter,
 			setCounter,
+			increment,
 		}}>
 			{children}
 		</CounterContext.Provider>
